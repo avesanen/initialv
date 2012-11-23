2cf2c4207534d71be8ddba9c2303755085ed477f
Antti Vesanen <namochan@gmail.com>
2012-11-23 21:31:28 +0200

    Committing before experimental Box2D branch.

---
+++ www/js/game/particles.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Monolith
+ * Date: 23.11.2012
+ * Time: 20:50
+ * To change this template use File | Settings | File Templates.
+ */
+define(function () {
+    return {};
+});
